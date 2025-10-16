1570ae94ae0b5c8b710d1911fa529121bf87a945
DipanshuSehgal270 <dipanshusehgal88@gmail.com>
2025-10-16 17:46:25 +0530

    MileStone MVP

--- src/main/java/com/jts/movie/config/JWTService.java
+++ src/main/java/com/jts/movie/config/JWTService.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import io.jsonwebtoken.Claims;
@@ -15,8 +16,9 @@ import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JWTService {
-	
-	public static final String SECRET = "404D635166546A576E5A7234753778214125442A472D4B6150645267556B5870";
+
+	@Value("${jwt.secret}")
+	private String SECRET;
 	
 	public String extractUsername(String token) {
 		return extractClaim(token, Claims::getSubject);
